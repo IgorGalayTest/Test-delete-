b1902649a788e72ef48d1a65cc811208f9e1c31f
Igor Galay <igogalai@gmail.com>
2015-08-10 01:01:03 +0300

    Initial Commit

---
+++ gitTest_forDeleting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitTest_forDeleting
+//
+//  Created by Igor Galay on 10.08.15.
+//  Copyright (c) 2015 Igor Galay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
