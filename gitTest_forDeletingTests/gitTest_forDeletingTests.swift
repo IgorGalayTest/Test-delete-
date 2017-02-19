dc466141384149f2fc3c1f8052942b70bc3664e9
Igor Galay <igogalai@gmail.com>
2017-02-19 21:49:41 +0300

    Swift 3 migration

--- gitTest_forDeletingTests/gitTest_forDeletingTests.swift
+++ gitTest_forDeletingTests/gitTest_forDeletingTests.swift
@@ -28,7 +28,7 @@ class gitTest_forDeletingTests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock() {
+        self.measure() {
             // Put the code you want to measure the time of here.
         }
     }
